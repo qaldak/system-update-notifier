fd7333a075f1fc2b36f2a82e62cf994dc803a870
Qaldak <qaldak@gmail.com>
2023-04-01 18:09:21 +0200

    tmp commit for init version

--- cmd/sysup-notifier/sysup-notifier.go
+++ cmd/sysup-notifier/sysup-notifier.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
+
+func main() {
+	fmt.Println(getFoo())
+
+	hasUpdates := determineUpdates()
+
+	if hasUpdates {
+		log.Println("Updates available.")
+	}
+}
 
 func getFoo() string {
 	return "Foo"
 }
 
-func main() {
-	fmt.Println(getFoo())
+func determineUpdates() bool {
+	_, err := os.Stat("testdata/.update_available")
+	if err != nil {
+		log.Println("No dietpi updates available.")
+		_, err := os.Stat("testdata/.apt_updates")
+		if err != nil {
+			log.Println("No apt updates available.")
+			return false
+		}
+	}
+	return true
 }
--- cmd/sysup-notifier/sysup-notifier_test.go
+++ cmd/sysup-notifier/sysup-notifier_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFoo(t *testing.T) {
 	got := getFoo()
@@ -9,5 +11,20 @@ func TestFoo(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
 
+func Test_determineUpdates(t *testing.T) {
+	tests := []struct {
+		name string
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := determineUpdates(); got != tt.want {
+				t.Errorf("determineUpdates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
