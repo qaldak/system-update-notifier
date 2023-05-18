d38552dad590c1009e76e8b3436397de222a9ba0
Qaldak <qaldak@gmail.com>
2023-05-18 23:38:13 +0200

    implementation input args

--- cmd/sysup-notifier/sysup-notifier.go
+++ cmd/sysup-notifier/sysup-notifier.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"sysup-notifier/internal/pub"
 	"sysup-notifier/internal/syschk"
 	"sysup-notifier/internal/utils/logger"
@@ -18,8 +20,38 @@ func main() {
 }
 
 func init() {
+	// input args declaration
+	var nolog bool
+	var debug bool
+
+	// flags declaration using flag package
+	flag.CommandLine.BoolVar(&nolog, "nl", false, "")
+	flag.CommandLine.BoolVar(&nolog, "nolog", false, "--nolog disables logging")
+	flag.CommandLine.BoolVar(&debug, "d", false, "Specify pass. Default is password")
+	flag.CommandLine.BoolVar(&debug, "debug", false, "Specify pass. Default is password")
+
+	flag.Parse() // after declaring flags we need to call it
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "nolog" || f.Name == "nl" {
+			nolog = true
+		}
+	})
+
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Name == "debug" || f.Name == "d" {
+			nolog = true
+		}
+	})
+
+
+
+	log.Println()
+	log.Printf("nolog: %v", nolog)
+	log.Printf("debug: %v", debug)
+
 	// Todo: check and map input args.
-	debug := false
+	// debug := true
 
 	// initialize logger
 	logger.InitLogger(debug)
